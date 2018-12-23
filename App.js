4229a58580e040dc6e2244e08068f5c0d906fcc5
omerom <omerom@200apps.co>
2018-12-23 21:05:34 +0200

    move App.js out off App folder for cloning and running with expo

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { createAppContainer, createStackNavigator} from 'react-navigation';
 
-import LoginScreen from './Screens/LoginScreen'
-import CollectionScreen from './Screens/CollectionScreen'
+import LoginScreen from './App/Screens/LoginScreen'
+import CollectionScreen from './App/Screens/CollectionScreen'
 import React from "react";
-import AlbumScreen from "./Screens/AlbumScreen";
+import AlbumScreen from "./App/Screens/AlbumScreen";
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import * as reducers from "./Reducers";
+import * as reducers from "./App/Reducers";
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
